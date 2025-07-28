20439d155f854b596a97175c97573776d7c658c1
Rahul Gupta <34992405+rahulgupta-rg@users.noreply.github.com>
2025-07-28 11:03:47 +0530

    Create 1971-FindifPathExistsinGraph

---
+++ 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
@@ -0,0 +1,32 @@
+// Last updated: 7/28/2025, 11:03:45 AM
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean[] visited = new boolean[n];
+        boolean result = false;
+        Queue<Integer> q = new LinkedList<>();
+        //prepare adj list
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(); 
+        for(int i = 0 ; i < n ; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0 ; i < edges.length; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        q.add(source);
+        visited[source] = true;
+        while(!q.isEmpty()) {
+            int node = q.remove();
+            for(int i : adj.get(node)) {
+                if(!visited[i]) {
+                    q.add(i);
+                    visited[i] = true;
+                }
+            }
+            System.out.println("Inside loop");
+        }
+        
+        return visited[destination];
+        
+    }
+}
