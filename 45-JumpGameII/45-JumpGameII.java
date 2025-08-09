b284a050b35ee8ca83c18fcf96115ca91fad4b99
Rahul Gupta <34992405+rahulgupta-rg@users.noreply.github.com>
2025-08-09 18:50:55 +0530

    File updated at 8/9/2025, 6:50:55 PM

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,49 +1,23 @@
-// Last updated: 8/9/2025, 6:25:33 PM
+// Last updated: 8/9/2025, 6:50:55 PM
 class Solution {
-    public int jump(int[] nums) {
+    public int maxArea(int[] height) {
 
-        if (nums == null || nums.length <= 1) 
-            return 0;
-
-        int jumps = 0;
-        int end = 0;       // end of current window (reachable with current jumps)
-        int farthest = 0;  // farthest reachable by taking one more jump from current window
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]);
-
-            // when we reach the end of the current window, we must make a jump
-            if (i == end) {
-                jumps++;
-                end = farthest;
-                // optional: early exit if already can reach or exceed last index
-                if (end >= nums.length - 1) 
-                    break;
+        int maxArea = -1;
+        int start = 0;
+        int end =height.length-1;
+        while(start<=end){
+            int leftHeight = height[start];
+            int rightHeight = height[end];
+            int curArea = Math.min(leftHeight, rightHeight) * (end-start);
+            maxArea = Math.max(curArea,maxArea);
+            if(leftHeight < rightHeight) {
+                start++;
+            } else {
+                end--;
             }
-        }
 
-        return jumps;
+        }
+        return maxArea;
         
     }
-
-    // public boolean canJump(int[] arr) {
-    //     return helper(0,arr);
-    // }
-
-    // boolean helper(int index , int[] arr) {
-    //     if(index==arr.length-1) {
-    //         return true;
-    //     }
-    //     if(index>arr.length-1)
-    //     {
-    //         return false;
-    //     }
-    //     for(int i = index;i<=arr[index];i++) {
-    //         boolean result = helper(index+i,arr );
-    //         if(result){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
 }
