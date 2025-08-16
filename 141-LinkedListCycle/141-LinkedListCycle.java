66c190f4e31ef3bd35d14b4525c0a5fc4b149b4e
Rahul Gupta <34992405+rahulgupta-rg@users.noreply.github.com>
2025-08-16 19:13:42 +0530

    File updated at 8/16/2025, 7:13:41 PM

--- 141-LinkedListCycle/141-LinkedListCycle.java
+++ 141-LinkedListCycle/141-LinkedListCycle.java
@@ -1,26 +1,42 @@
-// Last updated: 8/16/2025, 6:44:33 PM
+// Last updated: 8/16/2025, 7:13:41 PM
 /**
  * Definition for singly-linked list.
- * class ListNode {
+ * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast!=null && fast.next!=null) {
-            slow =slow.next;
-            fast=fast.next.next;
-            if(slow==fast){
-                return true;
-            }
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        
+        ListNode head = new ListNode(0);
+        ListNode tail=head;
+        int carry = 0;
+
+        while(l1!=null || l2!=null || carry!=0) {
+            
+            int digit1 = l1!=null ? l1.val : 0;
+            int digit2 = l2!=null ? l2.val : 0;
+            int sum = digit1+digit2+carry;
+            int nodeVal=sum%10;
+            ListNode newNode = new ListNode(nodeVal);
+            tail.next=newNode;
+            tail=tail.next;
+            carry=sum/10;
+
+
+            l1=l1!=null ? l1.next: null;
+            l2=l2!=null ? l2.next: null;
         }
-        return false;
+        return head.next;
     }
+
+    //9,9,9,9,9,9,9
+    //9,9,9,9
+    
+    //8,9,9,9,0,0,0,11
 }
