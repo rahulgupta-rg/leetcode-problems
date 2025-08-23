84ab09806791cfd10184c46fbfdf06bcb788fb5c
Rahul Gupta <34992405+rahulgupta-rg@users.noreply.github.com>
2025-08-23 10:15:42 +0530

    File updated at 8/23/2025, 10:15:42 AM

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,42 +1,21 @@
-// Last updated: 8/23/2025, 10:13:40 AM
+// Last updated: 8/23/2025, 10:15:42 AM
 class Solution {
     public void rotate(int[] nums, int k) {
-        //1,2,3,4,5,6,7
-        //3,4,5,6,7,1,2
-        
-        //3,2,1,4,5,6,7
-        //3,4,1,2,5,6,7
-        //3,4,5,2,1,6,7
-        //3,4,5,6,1,2,7
-        //3,4,5,6,7,2,1
-        
-        //1,2,3,4,5,6,7, 8
-        //4,5,6,1,2,3,7,8
-        //4,5,6,7,8,3,1,2
-        // if (nums.length == 0){
-        //     return;
-        // }
-        // if (nums.length == 1){
-        //     return;
-        // }
-        // if (nums.length < k) {
-        //     return;
-        // }
-        k = k % nums.length;
-        reverse(0, nums.length-1, nums);
-        reverse(0, k-1, nums);
-        reverse(k, nums.length-1, nums);
-        
-        
+        int n = nums.length;
+        k %= n; 
+
+        reverse(nums, 0, n - 1);  
+        reverse(nums, 0, k - 1);   
+        reverse(nums, k, n - 1);   
     }
 
-    public void reverse(int start , int end, int[] n) {
-       while(start < end){
-            int temp = n[end];
-            n[end] = n[start];
-            n[start] = temp;
-           start++;
-           end--;
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
     }
 }
